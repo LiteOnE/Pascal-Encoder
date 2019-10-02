e407b408c5721e2b2f9e792513d41857602a437d
Roman <r89534217610@gmail.com>
2019-10-02 18:45:35 +0300

    seg

--- main.js
+++ main.js
@@ -3,13 +3,20 @@ $(() => {
         var sourceText = $('#source-text').val();
         var encodedText = '';
 
+        var paramOffset = 1;
+
+        for (var c in $('[type=checkbox]')) {
+            if ($('[type=checkbox]')[c].checked)
+                paramOffset += c;
+        }
+
         var sourceLen = sourceText.length;
 
         let charSum = 0;
         for (var i in sourceText) {
             let curCharCode = sourceText.charCodeAt(i);
             charSum += curCharCode;
-            encodedText += String.fromCharCode(curCharCode ^ ((sourceLen + i + sourceText.charCodeAt(Math.max(0, i - 1))) % 255));
+            encodedText += String.fromCharCode(curCharCode ^ ((sourceLen + charSum + i + sourceText.charCodeAt(Math.max(0, i - 1)) + paramOffset) % (1 << 12)));
         }
 
         $('#encoded-text').val(encodedText);
