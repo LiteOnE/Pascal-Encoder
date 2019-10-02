d87ec091384cd5148e9fe6409ba42cf96ac64f0c
Roman <r89534217610@gmail.com>
2019-10-02 17:57:31 +0300

    Update algo

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ $(() => {
         var sourceLen = sourceText.length;
 
         for (var i in sourceText) {
-            encodedText += String.fromCharCode(sourceText.charCodeAt(i) ^ (sourceLen + i % 255));
+            encodedText += String.fromCharCode(sourceText.charCodeAt(i) ^ ((sourceLen + i + sourceText.charCodeAt(Math.max(0, i - 1))) % 255));
         }
 
         $('#encoded-text').val(encodedText);
